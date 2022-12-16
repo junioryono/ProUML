d922744b57dce1aaa7a29868a8ede116546d2fb5
Junior Yono <jryono123@gmail.com>
2022-12-16 00:02:21 +0000

    Remove frontend from router

--- backend/go.mod
+++ backend/go.mod
@@ -3,9 +3,8 @@ module github.com/junioryono/ProUML/backend
 go 1.19
 
 require (
-	github.com/gofiber/fiber/v2 v2.39.0
-	github.com/gofiber/template v1.7.1
-	github.com/gofiber/websocket/v2 v2.1.1
+	github.com/gofiber/fiber/v2 v2.40.1
+	github.com/gofiber/websocket/v2 v2.1.2
 	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.4.0
 	github.com/nedpals/supabase-go v0.2.0
@@ -13,6 +12,7 @@ require (
 
 require (
 	github.com/fasthttp/websocket v1.5.0 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
@@ -20,16 +20,18 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/nedpals/postgrest-go v0.1.3 // indirect
-	github.com/rivo/uniseg v0.4.2 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/savsgio/gotils v0.0.0-20220530130905-52f3993e8d6d // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/json-iterator/go v1.1.12
-	github.com/klauspost/compress v1.15.12 // indirect
+	github.com/klauspost/compress v1.15.13 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.41.0 // indirect
+	github.com/valyala/fasthttp v1.43.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"errors"
-	"html/template"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/gofiber/template/html"
 	"github.com/gofiber/websocket/v2"
 	"github.com/joho/godotenv"
 	"github.com/junioryono/ProUML/backend/transpiler"
@@ -19,173 +16,69 @@ import (
 )
 
 var (
-	supabaseUrl    string
-	supabaseKey    string
-	htmlProperties = map[string]map[string]string{
-		"Invalid URL": {"Title": "ProUML - Not Found"},
-		"/":           {"Title": "ProUML - Home"},
-		"/dashboard":  {"Title": "ProUML - Dashboard"},
-	}
+	SupabaseClient *supabase.Client
 )
 
-var (
-	SupabaseClient *supabase.Client
-	ReactBuildPath = "./frontend/build"
-	Production     = os.Getenv("production") == "true"
-)
+func main() {
+	InitSupabaseClient()
+	InitRouter()
+}
 
 func InitSupabaseClient() {
-	godotenv.Load("./.env")
-	supabaseUrl = os.Getenv("supabaseUrl")
-	supabaseKey = os.Getenv("supabaseKey")
+	godotenv.Load(".env")
+	supabaseUrl := os.Getenv("supabaseUrl")
+	supabaseKey := os.Getenv("supabaseKey")
 
 	if supabaseUrl == "" || supabaseKey == "" {
 		panic(errors.New("environment variables not found"))
 	}
 
 	SupabaseClient = supabase.CreateClient(supabaseUrl, supabaseKey)
-}
-
-func ConfigureCors() func(*fiber.Ctx) error {
-	return cors.New(cors.Config{
-		AllowCredentials: true,
-		AllowOrigins:     "https://prouml.com",
-		AllowHeaders:     "Origin, Content-Type, Accept, Accept-Language, Content-Length",
-	})
-}
-
-func getInnerHtmlProperty(page, property string) string {
-	if pageString, ok := htmlProperties[page]; ok {
-		if propertyString, ok := pageString[property]; ok {
-			return propertyString
-		}
-
-		goto exit
-	}
-
-exit:
-	return htmlProperties["Invalid URL"][property]
-}
-
-func SendIndexHTML(c *fiber.Ctx) error {
-	fiberMap := fiber.Map{
-		"Title": "<title>" + getInnerHtmlProperty(c.Path(), "Title") + "</title>",
-	}
-
-	return c.Status(fiber.StatusOK).Render("index", fiberMap)
-}
-
-func API_ToJSON() func(c *fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-		json, err := transpiler.ToJson(SupabaseClient, "", "") // DELETE THIS
-		if err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"success": false,
-				"reason":  "Could not transpile.",
-			})
-		}
-
-		return c.Status(fiber.StatusOK).Send(json)
+	if SupabaseClient == nil {
+		panic(errors.New("could not initialize supabase client"))
 	}
 }
 
-func HandleAPIRoutes(APIRouter fiber.Router) {
-	APIRouter.Get("/to-json", API_ToJSON())
-}
-
-func HandleDefaultRoutes(c *fiber.Ctx) error {
-	// If it's an API route
-	// fmt.Print(c.Path())
-
-	if strings.HasPrefix(c.Path(), "/api") {
-
-		// /transpile
-		// 	transpiler.ToJson()
-
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"success": false,
-			"reason":  "Invalid route.",
-		})
-	}
-
-	// If the request method is not GET
-	if c.Method() != "GET" {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"success": false,
-			"reason":  "Invalid request.",
-		})
-	}
-
-	// If file ends with .html, redirect to directory path; /directory/index.html => /directory
-	if strings.HasSuffix(c.Path(), ".html") {
-		return c.Redirect(c.BaseURL() + c.Path()[:strings.LastIndex(c.Path(), "/")])
-	}
-
-	// c.Response().Header.Add("Cache-Time", "6000")
-
-	// If the file exists in the frontend build directory
-	if _, err := os.Stat(ReactBuildPath + c.Path()); c.Path() != "/" && !os.IsNotExist(err) {
-		return c.Status(fiber.StatusOK).SendFile(ReactBuildPath + c.Path())
-	}
-
-	// Otherwise return React's index html
-	return SendIndexHTML(c)
-}
-
-func main() {
-	InitSupabaseClient()
-
+func InitRouter() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "5000"
 	}
 
-	if !Production {
-		ReactBuildPath = "../frontend/build"
-	}
-
-	htmlEngine := html.New(ReactBuildPath+"/", ".html")
-	htmlEngine.AddFunc(
-		// add unescape function
-		"unescape", func(s string) template.HTML {
-			return template.HTML(s)
-		},
-	)
-
-	if !Production {
-		htmlEngine.Reload(true)
-	}
-
-	MainRouter := fiber.New(fiber.Config{
+	Router := fiber.New(fiber.Config{
 		AppName:                 "ProUML",
 		EnableTrustedProxyCheck: true,
-		Views:                   htmlEngine,
 	})
 
-	APIRouter := MainRouter.Group("/api")
-	HandleAPIRoutes(APIRouter)
-
-	MainRouter.Use(recover.New())
-	// MainRouter.Use(cache.New(cache.Config{
-	// 	ExpirationGenerator: func(c *fiber.Ctx, cfg *cache.Config) time.Duration {
-	// 		newCacheTime, err := strconv.Atoi(c.GetRespHeader("Cache-Time", "600"))
-	// 		if err != nil {
-	// 			newCacheTime = 0
-	// 		}
-	// 		return time.Second * time.Duration(newCacheTime)
-	// 	},
-	// 	KeyGenerator: func(c *fiber.Ctx) string {
-	// 		return c.Path()
-	// 	},
-	// }))
-	MainRouter.Use(ConfigureCors())
-	MainRouter.Use(compress.New(compress.Config{
+	Router.Use(compress.New(compress.Config{
 		Level: compress.LevelBestCompression,
 	}))
-	APIRouter.Use(recover.New())
-	APIRouter.Use(ConfigureCors())
 
-	MainRouter.Use("/ws", func(c *fiber.Ctx) error {
+	Router.Use(recover.New())
+	Router.Use(ConfigureCors())
+	Router.Get("/test", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"status": "true",
+		})
+	})
+
+	HandleRoutes(Router)
+
+	log.Fatal(Router.Listen(":" + port))
+}
+
+func ConfigureCors() func(*fiber.Ctx) error {
+	return cors.New(cors.Config{
+		AllowCredentials: true,
+		AllowOrigins:     "https://api.prouml.com",
+		AllowHeaders:     "Origin, Content-Type, Accept, Accept-Language, Content-Length",
+	})
+}
+
+func HandleRoutes(Router fiber.Router) {
+	Router.Get("/to-json", API_ToJSON())
+
+	Router.Use("/ws", func(c *fiber.Ctx) error {
 		// IsWebSocketUpgrade returns true if the client
 		// requested upgrade to the WebSocket protocol.
 		if websocket.IsWebSocketUpgrade(c) {
@@ -195,7 +88,7 @@ func main() {
 		return fiber.ErrUpgradeRequired
 	})
 
-	MainRouter.Get("/ws/:id", websocket.New(func(wc *websocket.Conn) {
+	Router.Get("/ws/:id", websocket.New(func(wc *websocket.Conn) {
 		var (
 			mt  int
 			msg []byte
@@ -224,7 +117,18 @@ func main() {
 
 	}))
 
-	MainRouter.Use(HandleDefaultRoutes)
+}
 
-	log.Fatal(MainRouter.Listen(":" + port))
+func API_ToJSON() func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		json, err := transpiler.ToJson(SupabaseClient, "", "") // DELETE THIS
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"success": false,
+				"reason":  "Could not transpile.",
+			})
+		}
+
+		return c.Status(fiber.StatusOK).Send(json)
+	}
 }
