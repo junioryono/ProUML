296915346393fb1a6ecddf3864a2abfce9cf3a67
Corey Taylor <taylorcoreyc@gmail.com>
2023-02-19 12:13:05 -0800

    Changed share page

--- frontend/components/diagram/header/share-button.tsx
+++ frontend/components/diagram/header/share-button.tsx
@@ -103,7 +103,7 @@ export default function ShareButton({ user, diagram }: { user: User; diagram: Di
                                        disabled={isLoading}
                                        {...register("email")}
                                     />
-                                    <div className="flex flex-row hover:bg-slate-100 my-3 pl-2 cursor-pointer">
+                                    <div className="flex flex-row hover:bg-slate-50 my-2 pl-2 cursor-pointer">
                                        Editor
                                        <svg
                                           width="24"
@@ -129,9 +129,89 @@ export default function ShareButton({ user, diagram }: { user: User; diagram: Di
                                  </div>
                               </div>
 
-                              <div className="bg-white px-6 pt-2 pb-4 ml-4 flex flex-col">
-                                 <div>Hello1</div>
-                                 <div>Hello2</div>
+                              <div className="bg-white flex flex-row">
+                                 <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="40"
+                                    height="40"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    className="mt-2 ml-3"
+                                 >
+                                    <path
+                                       opacity="0.4"
+                                       d="M12 22.01C17.5228 22.01 22 17.5329 22 12.01C22 6.48716 17.5228 2.01001 12 2.01001C6.47715 2.01001 2 6.48716 2 12.01C2 17.5329 6.47715 22.01 12 22.01Z"
+                                       fill="#292D32"
+                                    />
+                                    <path
+                                       d="M12 6.93994C9.93 6.93994 8.25 8.61994 8.25 10.6899C8.25 12.7199 9.84 14.3699 11.95 14.4299C11.98 14.4299 12.02 14.4299 12.04 14.4299C12.06 14.4299 12.09 14.4299 12.11 14.4299C12.12 14.4299 12.13 14.4299 12.13 14.4299C14.15 14.3599 15.74 12.7199 15.75 10.6899C15.75 8.61994 14.07 6.93994 12 6.93994Z"
+                                       fill="#292D32"
+                                    />
+                                    <path
+                                       d="M18.7807 19.36C17.0007 21 14.6207 22.01 12.0007 22.01C9.3807 22.01 7.0007 21 5.2207 19.36C5.4607 18.45 6.1107 17.62 7.0607 16.98C9.7907 15.16 14.2307 15.16 16.9407 16.98C17.9007 17.62 18.5407 18.45 18.7807 19.36Z"
+                                       fill="#292D32"
+                                    />
+                                 </svg>
+                                 <div className="flex flex-col">
+                                    <div className="flex flex-row pt-2 pl-2">
+                                       First Last1
+                                       <div className="flex flex-row text-gray-600 hover:bg-slate-50 hover:text-black pl-3 cursor-pointer pl-auto  mr-1 rounded">
+                                          Editor
+                                          <svg
+                                             width="24"
+                                             height="24"
+                                             viewBox="0 0 24 24"
+                                             focusable="false"
+                                             className="cursor-pointer fill-slate-500"
+                                          >
+                                             <path d="M7 10l5 5 5-5H7z"></path>
+                                          </svg>
+                                       </div>
+                                    </div>
+                                    <div className="text-xs text-stone-500 pb-1 pl-2">name@example.com</div>
+                                 </div>
+                              </div>
+                              <div className="bg-white pb-4 flex flex-row">
+                                 <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="40"
+                                    height="40"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    className="mt-2 ml-3"
+                                 >
+                                    <path
+                                       opacity="0.4"
+                                       d="M12 22.01C17.5228 22.01 22 17.5329 22 12.01C22 6.48716 17.5228 2.01001 12 2.01001C6.47715 2.01001 2 6.48716 2 12.01C2 17.5329 6.47715 22.01 12 22.01Z"
+                                       fill="#292D32"
+                                    />
+                                    <path
+                                       d="M12 6.93994C9.93 6.93994 8.25 8.61994 8.25 10.6899C8.25 12.7199 9.84 14.3699 11.95 14.4299C11.98 14.4299 12.02 14.4299 12.04 14.4299C12.06 14.4299 12.09 14.4299 12.11 14.4299C12.12 14.4299 12.13 14.4299 12.13 14.4299C14.15 14.3599 15.74 12.7199 15.75 10.6899C15.75 8.61994 14.07 6.93994 12 6.93994Z"
+                                       fill="#292D32"
+                                    />
+                                    <path
+                                       d="M18.7807 19.36C17.0007 21 14.6207 22.01 12.0007 22.01C9.3807 22.01 7.0007 21 5.2207 19.36C5.4607 18.45 6.1107 17.62 7.0607 16.98C9.7907 15.16 14.2307 15.16 16.9407 16.98C17.9007 17.62 18.5407 18.45 18.7807 19.36Z"
+                                       fill="#292D32"
+                                    />
+                                 </svg>
+                                 <div className="flex flex-col pb-2">
+                                    <div className="flex flex-row pt-2 pl-2">
+                                       First Last2
+                                       <div className="flex flex-row text-gray-600 hover:bg-slate-50 hover:text-black pl-3 cursor-pointer pl-auto  mr-1 rounded">
+                                          Editor
+                                          <svg
+                                             width="24"
+                                             height="24"
+                                             viewBox="0 0 24 24"
+                                             focusable="false"
+                                             className="cursor-pointer fill-slate-500"
+                                          >
+                                             <path d="M7 10l5 5 5-5H7z"></path>
+                                          </svg>
+                                       </div>
+                                    </div>
+                                    <div className="text-xs text-stone-500 pb-1 pl-2">name@example.com</div>
+                                 </div>
                               </div>
 
                               <div className="bg-white px-4 sm:px-6">
@@ -144,29 +224,27 @@ export default function ShareButton({ user, diagram }: { user: User; diagram: Di
                                  </div>
                               </div>
 
-                              <div className="bg-white px-6 pt-2 pb-4 ml-4 mr-4 flex flex-row hover:bg-slate-300 rounded-md">
+                              <div className="bg-white px-6 mx-2 mb-4 mt-1 flex flex-row hover:bg-slate-100 rounded-l-full rounded-md">
                                  <svg
                                     focusable="false"
-                                    width="20"
-                                    height="20"
+                                    width="24"
+                                    height="24"
                                     viewBox="0 0 24 24"
-                                    className="rounded-md mt-2 bg-slate-400 hover:bg-slate-50"
+                                    className="rounded-md mt-2.5 bg-slate-100 hover:bg-slate-50"
                                  >
                                     <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
                                  </svg>
                                  <div className=" flex flex-col cursor-pointer rounded-md px-2 mb-1">
-                                    Restricted
-                                    <div className="text-xs text-stone-500">Only people with access can open the link</div>
+                                    <div className="flex flex-row text-sm cursor-pointer hover:bg-slate-200 w-28 pl-2 mt-1 rounded-md">
+                                       Restricted
+                                       <svg width="20" height="20" viewBox="0 0 24 24" focusable="false" className="">
+                                          <path d="M7 10l5 5 5-5H7z"></path>
+                                       </svg>
+                                    </div>
+                                    <div className="text-xs text-stone-500  pl-2">
+                                       Only people with access can open the link
+                                    </div>
                                  </div>
-                                 <svg
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    focusable="false"
-                                    className="cursor-pointer hover:bg-slate-400"
-                                 >
-                                    <path d="M7 10l5 5 5-5H7z"></path>
-                                 </svg>
                               </div>
 
                               <div className="bg-gray-50 px-4 py-3 flex flex-row sm:flex-row-reverse sm:px-6">
