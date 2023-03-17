3edeaf8641c04b5581886f3b8e1b9bbf0b4e33d5
Junior Yono <jryono123@gmail.com>
2023-03-16 21:16:40 -0700

    Shorten JPEG stylesheet

--- frontend/components/diagram/x6/graph/listeners/graph.ts
+++ frontend/components/diagram/x6/graph/listeners/graph.ts
@@ -76,127 +76,21 @@ export default function Graph(
             }
          },
          stylesheet: `
-         .user-cell-selection {
-            display: none;
-         }
-         .x6-port {
-            display: none;
-         }
          *, ::before, ::after {
             box-sizing: border-box;
          }
-         .x6-graph-scroller {
-            position: relative;
-            box-sizing: border-box;
-            overflow: scroll;
-            outline: none;
-         }
-         .x6-graph-scroller-content {
-            position: relative;
-         }
-         .x6-graph-scroller-background {
-            position: absolute;
-            top: 0;
-            right: 0;
-            bottom: 0;
-            left: 0;
-         }
-         .x6-graph-scroller .x6-graph {
-            position: absolute;
-            display: inline-block;
-            margin: 0;
-            box-shadow: none;
-         }
-         .x6-graph-scroller .x6-graph > svg {
-            display: block;
-         }
-         .x6-graph-scroller.x6-graph-scroller-paged .x6-graph {
-            box-shadow: 0 0 4px 0 #eee;
-         }
-         .x6-graph-scroller.x6-graph-scroller-pannable[data-panning='false'] {
-            cursor: grab;
-         }
-         .x6-graph-scroller.x6-graph-scroller-pannable[data-panning='true'] {
-            cursor: grabbing;
-            user-select: none;
-         }
-         .x6-graph-pagebreak {
-            position: absolute;
-            top: 0;
-            right: 0;
-            bottom: 0;
-            left: 0;
-         }
-         .x6-graph-pagebreak-vertical {
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            box-sizing: border-box;
-            width: 1px;
-            border-left: 1px dashed #bdbdbd;
-         }
-         .x6-graph-pagebreak-horizontal {
-            position: absolute;
-            right: 0;
-            left: 0;
-            box-sizing: border-box;
-            height: 1px;
-            border-top: 1px dashed #bdbdbd;
-         }
-         .x6-graph {
-            position: relative;
-            outline: none;
-            touch-action: none;
-          }
-         .x6-graph-background,
-         .x6-graph-grid,
-         .x6-graph-svg {
-            position: absolute;
-            top: 0;
-            right: 0;
-            bottom: 0;
-            left: 0;
-         }
-         .x6-graph-background-stage,
-         .x6-graph-grid-stage,
-         .x6-graph-svg-stage {
-            user-select: none;
-         }
-         .x6-graph.x6-graph-pannable {
-            cursor: grab;
-            cursor: -moz-grab;
-            cursor: -webkit-grab;
-         }
-         .x6-graph.x6-graph-panning {
-            cursor: grabbing;
-            cursor: -moz-grabbing;
-            cursor: -webkit-grabbing;
-            user-select: none;
-         }
-         .x6-node {
-            cursor: move;
-            /* stylelint-disable-next-line */
-         }
-         .x6-node.x6-node-immovable {
-            cursor: default;
-         }
-         .x6-node * {
-            -webkit-user-drag: none;
-         }
-         .x6-node .scalable * {
+         .x6-node .scalable *,
+         .x6-edge .source-marker,
+         .x6-edge .target-marker {
             vector-effect: non-scaling-stroke;
          }
-         .x6-node [magnet='true'] {
-            cursor: crosshair;
-            transition: opacity 0.3s;
-         }
-         .x6-node [magnet='true']:hover {
-            opacity: 0.7;
+         .x6-node foreignObject,
+         .x6-node foreignObject > body {
+            overflow: visible;
+            background-color: transparent;
          }
          .x6-node foreignObject {
             display: block;
-            overflow: visible;
-            background-color: transparent;
          }
          .x6-node foreignObject > body {
             position: static;
@@ -204,102 +98,31 @@ export default function Graph(
             height: 100%;
             margin: 0;
             padding: 0;
-            overflow: visible;
-            background-color: transparent;
          }
-         .x6-edge .source-marker,
-         .x6-edge .target-marker {
-            vector-effect: non-scaling-stroke;
-         }
-         .x6-edge .connection {
+         .x6-edge .connection,
+         .x6-edge .connection-wrap {
             stroke-linejoin: round;
             fill: none;
          }
          .x6-edge .connection-wrap {
-            cursor: move;
-            opacity: 0;
-            fill: none;
             stroke: #000;
             stroke-width: 15;
             stroke-linecap: round;
-            stroke-linejoin: round;
          }
-         .x6-edge .connection-wrap:hover {
-            opacity: 0.4;
-            stroke-opacity: 0.4;
-         }
-         .x6-edge .vertices {
-            cursor: move;
+         .x6-edge .vertices,
+         .x6-edge .arrowheads,
+         .x6-edge .connection-wrap {
             opacity: 0;
          }
-         .x6-edge .vertices .vertex {
-            fill: #1abc9c;
-         }
-         .x6-edge .vertices .vertex :hover {
-            fill: #34495e;
-            stroke: none;
-         }
-         .x6-edge .vertices .vertex-remove {
-            cursor: pointer;
-            fill: #fff;
-         }
-         .x6-edge .vertices .vertex-remove-area {
-            cursor: pointer;
-            opacity: 0.1;
-         }
-         .x6-edge .vertices .vertex-group:hover .vertex-remove-area {
-            opacity: 1;
-         }
-         .x6-edge .arrowheads {
-            cursor: move;
-            opacity: 0;
-         }
+         .x6-edge .vertices .vertex,
          .x6-edge .arrowheads .arrowhead {
             fill: #1abc9c;
          }
-         .x6-edge .arrowheads .arrowhead :hover {
-            fill: #f39c12;
-            stroke: none;
-         }
-         .x6-edge .tools {
-            cursor: pointer;
-            opacity: 0;
-         }
-         .x6-edge .tools .tool-options {
-            display: none;
-         }
-         .x6-edge .tools .tool-remove circle {
-            fill: #f00;
-         }
-         .x6-edge .tools .tool-remove path {
+         .x6-edge .vertices .vertex-remove {
             fill: #fff;
          }
-         .x6-edge:hover .vertices,
-         .x6-edge:hover .arrowheads,
-         .x6-edge:hover .tools {
-            opacity: 1;
-         }
-         .x6-highlight-opacity {
-            opacity: 0.3;
-         }
-         .x6-cell-tool-editor {
-            position: relative;
-            display: inline-block;
-            min-height: 1em;
-            margin: 0;
-            padding: 0;
-            line-height: 1;
-            white-space: normal;
-            text-align: center;
-            vertical-align: top;
-            overflow-wrap: normal;
-            outline: none;
-            transform-origin: 0 0;
-            -webkit-user-drag: none;
-         }
-         .x6-edge-tool-editor {
-            border: 1px solid #275fc5;
-            border-radius: 2px;
+         .x6-edge .vertices .vertex-remove-area {
+            opacity: 0.1;
          }
          `,
       });
