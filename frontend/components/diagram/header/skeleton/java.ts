9ba0e14bf1413e0a9af027658e506b9db14a9e40
Junior Yono <jryono123@gmail.com>
2023-05-05 13:28:45 -0700

    Fixed bugs

--- frontend/components/diagram/header/skeleton/java.ts
+++ frontend/components/diagram/header/skeleton/java.ts
@@ -7,12 +7,12 @@ export default async function DownloadJavaSkeleton(diagramName: string, cells: X
    // Get all the nodes from the graph
    const nodes = cells.filter((cell) => {
       return cell.isNode();
-   });
+   }) as X6Type.Node[];
 
    // Get all the edges from the graph
    const edges = cells.filter((cell) => {
       return cell.isEdge();
-   });
+   }) as X6Type.Edge[];
 
    const files = nodes.map((node) => {
       const { package: packageName, name, type, variables, methods } = node.getProp() as ClassNode;
@@ -25,10 +25,91 @@ export default async function DownloadJavaSkeleton(diagramName: string, cells: X
 
       code += `public class ${name} `;
 
-      if (type === "abstract") {
-         code += "extends ";
+      // An abstract class can extend another class and implement interfaces
+      // An interface can extend multiple interfaces (not implement)
+      // A regular class can extend another class and implement interfaces
+
+      const extendsNodes = edges.filter((edge) => {
+         const sourceId = edge.getSourceCellId();
+         if (sourceId !== node.id) {
+            return false;
+         }
+
+         const target = edge.getTargetCell();
+         if (!target) {
+            return false;
+         }
+
+         const targetNode = target as X6Type.Node;
+         const targetNodeProp = targetNode.getProp();
+         if (!targetNodeProp) {
+            return false;
+         }
+
+         return targetNodeProp.type === "abstract" && edge.getProp("edgeType") === "generalization";
+      });
+
+      const implementsNodes = edges.filter((edge) => {
+         const sourceId = edge.getSourceCellId();
+         if (sourceId !== node.id) {
+            return false;
+         }
+
+         const target = edge.getTargetCell();
+         if (!target) {
+            return false;
+         }
+
+         const targetNode = target as X6Type.Node;
+         const targetNodeProp = targetNode.getProp();
+         if (!targetNodeProp) {
+            return false;
+         }
+
+         console.log("targetNodeProp", targetNodeProp, edge.getProp("edgeType"));
+         return targetNodeProp.type === "interface" && edge.getProp("edgeType") === "realization";
+      });
+
+      console.log("name", name);
+      console.log("type", type);
+      console.log("extendsNodes", extendsNodes);
+      console.log("implementsNodes", implementsNodes);
+      if (!type || type === "abstract") {
+         if (extendsNodes.length > 0) {
+            code += `extends `;
+            code += extendsNodes
+               .map((edge) => {
+                  const target = edge.getTargetCell() as X6Type.Node;
+                  return target.getProp("name");
+               })
+               .join(", ");
+
+            code += " ";
+         }
+
+         if (implementsNodes.length > 0) {
+            code += `implements `;
+            code += implementsNodes
+               .map((edge) => {
+                  const target = edge.getTargetCell() as X6Type.Node;
+                  return target.getProp("name");
+               })
+               .join(", ");
+
+            code += " ";
+         }
       } else if (type === "interface") {
-         code += "implements ";
+         if (extendsNodes.length > 0) {
+            code += `extends `;
+            code += extendsNodes
+               .map((edge) => {
+                  const target = edge.getTargetCell() as X6Type.Node;
+                  return target.getProp("name");
+               })
+               .join(", ");
+
+            code += " ";
+         }
       }
 
       code += "{\n";
@@ -68,7 +149,37 @@ export default async function DownloadJavaSkeleton(diagramName: string, cells: X
 
       code += methods
          .map((method) => {
-            return `    ${method.type} ${method.name}() {}`;
+            let string = `    ${method.accessModifier || "public"} `;
+
+            if (method.static) {
+               string += "static ";
+            }
+
+            if (method.final) {
+               string += "final ";
+            }
+
+            if (method.type) {
+               string += `${method.type} `;
+            }
+
+            string += `${method.name}(`;
+
+            string += method.parameters
+               .map((parameter) => {
+                  return `${parameter.type} ${parameter.name}`;
+               })
+               .join(", ");
+
+            string += ")";
+
+            if (method.abstract) {
+               string += ";";
+            } else {
+               string += " {\n        // TODO: Implement this method\n    }";
+            }
+
+            return string;
          })
          .join("\n");
 
